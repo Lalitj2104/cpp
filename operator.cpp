4bc77f38a8c6dc68a1d2e4355820ef619f89c10a
Lalit2104 <lalitjindal0295@gmail.com>
2023-08-21 12:23:45 +0530

    committed

--- operator.cpp
+++ operator.cpp
@@ -13,4 +13,10 @@ int main()
     cout << "~num1 :" << (~num1) << endl;
     cout << "num1<<num2 :" << (num1 << num2) << endl;
     cout << "num1>>num2 :" << (num1 >> num2) << endl;
+
+
+    cout<<(++num1)<<endl;
+    cout<<(num1++)<<endl;
+    cout<<(--num1)<<endl;
+    cout<<(num1--)<<endl;
 }
