c54d8406a1f2c40cd1915f3e9725cba95ed1a1c1
Lalit2104 <lalitjindal0295@gmail.com>
2023-09-25 22:51:12 +0530

    commited

---
+++ binary search/upperlower.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void search(int arr[],int size,int key){
+    int start,end,mid;
+    start=0;
+    end=size-1;
+    mid=start+((end-start)/2);
+    while(start<end){
+        if(arr[mid]==key){
+
+            if(arr[mid+1]>key){
+                cout<<"last key element at: "<<(mid+1);
+            }
+            else if(arr[mid+1]==key){}
+                
+        }
+        else if(arr[mid]<key){
+            start=mid+1;
+            mid=start+((end-start)/2);
+
+
+        }
+        else if(arr[mid]>key){
+            end=mid-1;
+            mid=start+((end-start)/2);
+
+        }
+    }
+}
+
+int main(){
+    int size,key,arr[100];
+    cout<<"Enter size of array: ";
+    cin>>size;
+    cout<<"Enter the element of array: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>  arr[i];
+    }
+    cout<<"Enter key u want to search: "<<endl;
+    cin>>key;
+    search(arr,size,key);
+}
