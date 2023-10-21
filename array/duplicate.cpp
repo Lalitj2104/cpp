7c720f0e64fbefdf92ac705c6ce5a4556d34023f
Lalit2104 <lalitjindal0295@gmail.com>
2023-10-22 00:10:20 +0530

    commited

--- array/duplicate.cpp
+++ array/duplicate.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 using namespace std;
-int main(){
+int duplicate (int arr[],int size){
     
+}
+int main(){
+
+    int num, arr[100], size;
+    cout << "Enter number of test cases: " << endl;
+    cin >> num;
+    cout << "Enter number of elements in the array: " << endl;
+    cin >> size;
+    cout << "Enter elements :" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    int result=duplicate(arr,size);
+    cout<<"Duplicate element in the given array is: "<<result<<endl;
 }
