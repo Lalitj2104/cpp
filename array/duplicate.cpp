6ff3b60bbc99092cd8b47740880757116e420448
Lalit2104 <lalitjindal0295@gmail.com>
2023-10-22 00:21:23 +0530

    commited

--- array/duplicate.cpp
+++ array/duplicate.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 int duplicate (int arr[],int size){
-    
+    int ans=0;
+    for(int i=0;i<size;i++){
+        ans=ans^arr[i];
+    } 
+    for(int i=0;i<size;i++){
+        ans=ans^i;
+    }  
+    return ans;
+       
 }
 int main(){
 
