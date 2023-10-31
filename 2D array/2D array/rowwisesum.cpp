0474eb43395751fcd35bc1ad1bae5e2c4e2a248e
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-01 01:20:24 +0530

    commited

--- 2D array/2D array/rowwisesum.cpp
+++ 2D array/2D array/rowwisesum.cpp
@@ -3,24 +3,18 @@ using namespace std;
 
 void rowWiseSum(int arr[][100], int row, int column)
 {
-    int start = 0;
-    while (start < row)
-    {
+    
         for (int i = 0; i < row; i++)
         {
             int sum = 0;
             for (int j = 0; j < column; j++)
             {
-                
-                if (i == start)
                     sum += arr[i][j];
             }
-            cout<<"sum of "<<start<<"th row = "<<sum<<endl;
+            cout<<"sum of "<<row<<"th row = "<<sum<<endl;
             
-            start++;
         }
     }
-}
 int main()
 {
     int row, column, arr[100][100], target;
