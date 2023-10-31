5d44e419d4d733a12bd3354929fcc4ca526e00a6
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-01 00:21:52 +0530

    commited

---
+++ 2D array/2D array/output.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int row,column,arr[100][100];
+    cout<<"enter the elements u want to enter in the array: "<<endl;
+    cin>>row>>column;
+
+    cout<<"enter the elements in the array: ";
+//for inputing the values from the users
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+//for getting the output from the user
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+}
