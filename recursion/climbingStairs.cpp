d6ae00f4f66a6bb01f02bfc2b14288b4500778f7
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-02 15:20:44 +0530

    commited

---
+++ recursion/climbingStairs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int climbStairs(int num){
+    
+}
+
+int main(){
+    int num;
+    cout<<"enter the numbers of stairs u want to climb: "<<endl;
+    cin>>num;
+    int result=climbStairs(num);
+    cout<<result<<endl;
+}
---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int num)
+{
+    if(num==0){
+        return 0;
+    }
+    if(num==1){
+        return 1;
+    }
+    int result=fibonacci(num-1)+fibonacci(num-2);
+    return result;
+}
+int main(){
+    int num;
+    cout<<"Enter the number to which u want to find series: ";
+    cin>>num;
+    int result= fibonacci(num);
+    cout<<result;
+}
