2209e7516d929cbf2b28857d6b55582239fa510e
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-02 15:24:37 +0530

    commited

--- recursion/climbingStairs.cpp
+++ recursion/climbingStairs.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 int climbStairs(int num){
-    
+    if(num<0){
+        return 0;
+    }
+    if(num==0){
+        return 1;
+    }  
+    int result= climbStairs(num-1)+climbStairs(num-2) ;
+    return result;
 }
 
 int main(){
@@ -9,5 +16,6 @@ int main(){
     cout<<"enter the numbers of stairs u want to climb: "<<endl;
     cin>>num;
     int result=climbStairs(num);
+    cout<<"number of ways to climb is: ";
     cout<<result<<endl;
 }
