c890c1a76b56e7c00a14c1c1cf7cb703108a4025
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-05 23:03:14 +0530

    commited

--- 2D array/2D array/spiralPrint.cpp
+++ 2D array/2D array/spiralPrint.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void spiralPrint(){
-    
+void spiralPrint()
+{
 }
 
+int main()
+{
+    int row, column, arr[100][100];
+    cout << "enter the elements u want to enter in the array: " << endl;
+    cin >> row >> column;
 
-int main(){
-        int row,column,arr[100][100];
-    cout<<"enter the elements u want to enter in the array: "<<endl;
-    cin>>row>>column;
-
-    cout<<"enter the elements in the array: ";
-//for inputing the values from the users
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            cin>>arr[i][j];
+    cout << "enter the elements in the array: ";
+    // for inputing the values from the users
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cin >> arr[i][j];
         }
     }
 }
