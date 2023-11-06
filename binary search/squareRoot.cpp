c3db598d58c52df9a31a9ef56a172c6d405b2b5f
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-07 02:07:28 +0530

    commited

--- binary search/squareRoot.cpp
+++ binary search/squareRoot.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
-int squareRoot(){
-
+long long int squareRoot(int n)
+{
+    int s = 0;
+    int e = n ;
+    int mid = s + (e - s) / 2;
+    
+    long long int ans = -1;
+    while (s <= e)
+    {
+        long long int sq = mid * mid;
+        if ((sq) == n)
+        {
+            return mid;
+        }
+        else if ((sq) < n)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid-1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
 }
 
-int main(){
-    
+int main()
+{
+    int num;
+    cout << "Enter the number whose square root u want to find:";
+    cin >> num;
+    int result = squareRoot(num);
+    cout << result;
 }
