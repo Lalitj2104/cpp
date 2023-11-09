32b06a0bfbad5ba6999cefd430ceaf3e0ee7fafb
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-09 21:34:40 +0530

    commited

--- recursion/power1.cpp
+++ recursion/power1.cpp
@@ -1,75 +1,41 @@
 #include <iostream>
-#include<math.h>
+#include <math.h>
 using namespace std;
 
+int Power(int num, int power)
+{
 
-int Power(int num,int power){
-   
-    if(power==0){
-        return 1 ;
+    if (power == 0)
+    {
+        return 1;
     }
-    if(power==1){
+    if (power == 1)
+    {
         return num;
     }
-    int result=pow(num,power/2);
+    int result = pow(num, power / 2);
 
-    if(num%2==0){
-        return result*result;
+    if (num % 2 == 0)
+    {
+        return result * result;
     }
-    else{
-        return num*result*result;
+    else
+    {
+        return num * result * result;
     }
-
 }
 
-int main(){
-    int num,power;
-    cout<<"Enter the number: "<<endl;
-    cin >>num;
-    cout<<"Enter the power: "<<endl;
-    cin>>power;
+int main()
+{
+    int num, power;
+    cout << "Enter the number: " << endl;
+    cin >> num;
+    cout << "Enter the power: " << endl;
+    cin >> power;
 
-    int result= Power(num,power);
-    cout<<"result is:  "<<result;
+    int result = Power(num, power);
+    cout << "result is:  " << result;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // int   Power(int &num,int power){
 //     int result=num;
@@ -85,6 +51,5 @@ int main(){
 
 //     Power(num,power-1);
 //     cout<<result;
-
 
 // }
