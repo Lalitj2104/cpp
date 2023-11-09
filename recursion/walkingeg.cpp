e69863cb8d3440ec512074279a2fc10b2c302fde
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-09 21:36:15 +0530

    commited

--- recursion/walkingeg.cpp
+++ recursion/walkingeg.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
-void walking(int num,int i){
-    
-    if(i==(num+1)){
+void walking(int num, int i)
+{
+
+    if (i == (num + 1))
+    {
         return;
     }
-    cout<<i<<endl;
+    cout << i << endl;
     i++;
-    walking(num,i);
+    walking(num, i);
 }
-int main(){
-    int num, i=0;
-    cout<<"enter the steps to reach home: ";
-    cin>>num;
-    walking(num,i);
+int main()
+{
+    int num, i = 0;
+    cout << "enter the steps to reach home: ";
+    cin >> num;
+    walking(num, i);
 }
