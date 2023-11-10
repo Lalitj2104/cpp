62f2524659918427c175d91f4c8913bf08318086
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-10 23:36:50 +0530

    commited

--- recursion/issorted.cpp
+++ recursion/issorted.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 using namespace std;
-bool isSorted(int arr[],int size){
-    if(size==0||size==1){
+bool isSorted(int arr[], int size)
+{
+    if (size == 0 || size == 1)
+    {
         return 1;
     }
-    if(arr[0]>arr[1]){
+    if (arr[0] > arr[1])
+    {
         return 0;
     }
-    else{
-        int remain=isSorted(arr+1,size-1);
+    else
+    {
+        int remain = isSorted(arr + 1, size - 1);
         return remain;
     }
-
 }
 
-int main(){
-    int arr[100],size;
-    cout<<"enter the size of array: "<<endl;
-    cin>>size;
-    cout<<"enter the elements for the array: ";
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
+int main()
+{
+    int arr[100], size;
+    cout << "enter the size of array: " << endl;
+    cin >> size;
+    cout << "enter the elements for the array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
-    bool result=isSorted(arr,size);
-    if(result==1){
-        cout<<"sorted"<<endl;
-
+    bool result = isSorted(arr, size);
+    if (result == 1)
+    {
+        cout << "sorted" << endl;
     }
-    else{
-        cout<<"not sorted"<<endl;
-        
+    else
+    {
+        cout << "not sorted" << endl;
     }
 }
