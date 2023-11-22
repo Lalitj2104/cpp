68a3db67d864f279777d1eb5bb0f02113cb32cc7
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-22 10:32:51 +0530

    commited

--- binary search/BookAllocationProblem.cpp
+++ binary search/BookAllocationProblem.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
+int isPossible(int arr[],int books,int students,int mid){
+    int stdCount=1;
+    int pageSum=0;
+
+    for(int i=0;i<books;i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum+=arr[i];
+        }
+        else{
+            stdCount++;
+            if(stdCount>students ||arr[i]>mid ){
+                return false;
+            }
+            pageSum=arr[i];
+        }
+    }
+    return true;
+}
 void insertion(int arr[], int books)
 {
     cout << "Enter the pages in each book: " << endl;
@@ -18,19 +36,31 @@ int bookAllocation(int arr[], int books, int students)
         e += arr[i];
     }
     int s = 0;
+    int ans = -1;
     int mid = s + (e - s) / 2;
     while (s <= e)
     {
+        if(isPossible(arr,books,students,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
     }
+    return ans;
 }
 
 int main()
 {
 
     int arr[100], books, students;
-    cout << "Enter number pf books: " << endl;
+    cout << "Enter number of books: " << endl;
     cin >> books;
     cout << "Enter the number of students: " << endl;
     cin >> students;
     insertion(arr, books);
+    int result=bookAllocation(arr,books,students);
+    cout<<"the min division will be "<<result;
 }
---
+++ binary search/painter'spartion.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+void insertion(int arr[], int size)
+{
+    cout << "Enter the pages in each book: " << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+int isPartition(int arr[],int size,int painter,int mid){
+    
+
+}
+int partition(int arr[],int size,int painter){
+    int s=0;
+    int sum=0;
+    for(int i=0;i<size;i++){
+        sum+=arr[i];
+    }
+    int e=sum;
+    int mid = s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(isPartition(arr,size,painter,mid)){
+            ans=mid;
+            e=mid-1;
+
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+
+}
+
+
+int main(){
+    int arr[100],size,painter;
+    cout<<"Enter the number of walls: "<<endl;
+    cin>>size;
+    insertion(arr,size);
+    cout<<"Enter the number of painters:"<<endl;
+    cin>>painter;
+    int result=partition(arr,size,painter);
+}
