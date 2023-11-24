8c7491c72f58d021887c215ccd5ef81193886ac7
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-24 18:04:39 +0530

    commited

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void insertion(int arr[], int size)
+{
+    cout << "Enter the elements in array: " << endl;
 
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+void result(int arr[], int size)
+{
+    cout << "sorted array: " << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i]<<endl;
+    }
+}
+void bubbleSort(int arr[], int size)
+{
+    while (size--)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[100], size;
+    cout << "Enter the size of array: " << endl;
+    cin >> size;
+    insertion(arr, size);
+    bubbleSort(arr, size);
+    result(arr, size);
 }
