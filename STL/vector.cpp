c03b87395a0acb633ed90e0e1a91680a8ca36dcc
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-27 22:21:40 +0530

    commited

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -17,4 +17,19 @@ int main(){
     cout<<"capacity: "<<v.capacity()<<endl;
     cout<<"Size -> "<<v.size()<<endl;
 
+    cout<<"element at 1 index: "<<v.at(1)<<endl;
+
+    cout<<"first element: "<<v.front()<<endl;
+    cout<<"last element : "<<v.back()<<endl;
+
+
+    cout<<"before pop:";
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl;
+    v.pop_back();
+    cout<<"After pop:";
+    for(int i:v){
+        cout<<i<<" ";
+    }
 }
