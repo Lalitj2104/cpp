c18cc83d958d90de763c1e813dacd3ca6660a2e5
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-27 17:35:20 +0530

    commited

--- sorting/insertionSort.cpp
+++ sorting/insertionSort.cpp
@@ -20,3 +20,28 @@ void result(int arr[], int size)
         cout << arr[i]<<endl;
     }
 }
+
+
+void insertionSort(int arr[],int size){
+     for(int i=1;i<size;i++){
+        int temp=arr[i];
+        int j=i-1;
+          for(;j>=0;j--){
+            if(arr[j]>temp){
+                arr[j+1]= arr[j];
+            }
+            else{
+                break;
+            }
+          }
+          arr[j+1]=temp;
+     }
+}
+int main(){
+    int arr[100],size;
+    cout<<"Enter the size of array: "<<endl;
+    cin>>size;
+    insertion(arr,size);
+    insertionSort(arr,size);
+    result(arr,size);
+}
