ed36a85962ed7380e4f9c6fac64e9a32574196a8
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-27 18:25:42 +0530

    commited

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -2,5 +2,19 @@
 #include <vector>
 using namespace std;
 int main(){
-    
+    vector<int>v;
+    cout<<"capacity: "<<v.capacity()<<endl;
+
+    v.push_back(1);
+    cout<<"capacity: "<<v.capacity()<<endl;
+    cout<<"Size -> "<<v.size()<<endl;
+
+    v.push_back(2);
+    cout<<"capacity: "<<v.capacity()<<endl;
+
+
+    v.push_back(3);
+    cout<<"capacity: "<<v.capacity()<<endl;
+    cout<<"Size -> "<<v.size()<<endl;
+
 }
