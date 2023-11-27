4f8d5c92bc813a56fa9def2517342623364652a7
Lalit2104 <lalitjindal0295@gmail.com>
2023-11-27 18:09:11 +0530

    commited

---
+++ STL/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main(){
+    array<int,4> arr={1,2,3,4};
+
+    int size= arr.size();
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<endl;
+
+    }
+
+    cout<<arr.empty()<<endl;
+    cout<<"first element of array: "<<arr.front()<<endl;
+    cout<<"last element of array: "<<arr.back()<<endl;
+}
