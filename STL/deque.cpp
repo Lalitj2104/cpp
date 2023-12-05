b980315aefb1ec079b49e998c2418e081f03c740
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-05 11:04:26 +0530

    commited

--- STL/deque.cpp
+++ STL/deque.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <deque>
 using namespace std;
-int main(){
+int main()
+{
     deque<int> d;
 
     d.push_back(1);
     d.push_front(2);
-    cout<<"empty or not: "<<d.empty()<<endl;
-    cout<<"Element asked is :"<<d.at(1)<<endl;
-    cout<<"Element at start: "<<d.front()<<endl;
-    cout<<"Element at last: "<<d.back()<<endl;
+    cout << "empty or not: " << d.empty() << endl;
+    cout << "Element asked is :" << d.at(1) << endl;
+    cout << "Element at start: " << d.front() << endl;
+    cout << "Element at last: " << d.back() << endl;
 
-    cout<<"size before erase: "<<d.size()<<endl;
-    d.erase(d.begin(),d.begin()+1);
-    cout<<"size after erase: "<<d.size()<<endl;
-
-
+    cout << "size before erase: " << d.size() << endl;
+    d.erase(d.begin(), d.begin() + 1);
+    cout << "size after erase: " << d.size() << endl;
 }
