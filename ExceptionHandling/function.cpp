d2ace5c5929da7d908b06a40212d617aeb0515c9
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-08 22:10:26 +0530

    commited

--- ExceptionHandling/function.cpp
+++ ExceptionHandling/function.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int division(int x,int y){
-    if(y==0)
+int division(int x, int y)
+{
+    if (y == 0)
         throw 0;
     else
-        return x/y;
+        return x / y;
 }
 
-int main(){
-    int a=20,b=0,z;
+int main()
+{
+    int a = 20, b = 0, z;
 
-    try{
-        z=division(a,b);
-        cout<<z<<endl;
+    try
+    {
+        z = division(a, b);
+        cout << z << endl;
     }
-    catch(int q){
-        cout<<"Division by zero "<<q<<endl;
+    catch (int q)
+    {
+        cout << "Division by zero " << q << endl;
     }
 }
