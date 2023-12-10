7759adc9b3ff29fa9691f55cb05d2f24306a2cf3
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-10 18:23:33 +0530

    commited

---
+++ ETE/arraymerge.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int q,w;
+    cin>>q>>w;
+    int x=q+w;
+    int arr[x];
+    for(int i=0;i<q;i++){
+        cin>>arr[i];
+    }
+    for(int j=q;j<x;j++){
+        cin>>arr[j];
+    }
+    
+    sort(arr,arr+x);
+
+    for(int i=0;i<x;i++){
+        cout<<arr[i];
+    }
+}
