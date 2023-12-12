feb45ef54493b626770266901f955aa2647a489b
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-13 00:19:50 +0530

    commited

---
+++ ETE/longestcommonprefix.cpp
@@ -0,0 +1,33 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<string>a;
+    for(int i=0;i<3;i++){
+        string c;
+        cin>>c;
+        a.push_back(c);
+    }
+    int n;
+    n=a[0].size();
+    
+    for(int i=1;i<n;i++){
+        n=fmin(n,a[i].size());
+    }
+    string pre;
+    for(int i=0;i<n;i++){
+        char b=a[0][i];
+        bool flag=true;
+        for(int j=0;j<3;j++){
+            if(a[j][i]!=b){
+                flag=false;
+            }
+        }
+        if(flag==true) pre+=b;
+        else    break;
+    }
+    cout<<pre;
+}
