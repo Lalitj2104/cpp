34ce4d55b0b8765740e26de465c1a40b9d6d7194
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-12 10:57:13 +0530

    commited

---
+++ ETE/prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool flag=true;
+    for(int i=2;i<n;i++){
+        if (n%i==0)
+        {
+            flag=false;
+        }
+        
+    }
+
+    if(flag==true){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+
+}
