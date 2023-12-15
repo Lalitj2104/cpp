dace264bd6fd0a4befcb7ef8a6aef3b0c519334e
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-15 23:37:43 +0530

    commited

--- ETE/pattern54.cpp
+++ ETE/pattern54.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
-    cin>>num;
-    for(int i=1;i<=num;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
