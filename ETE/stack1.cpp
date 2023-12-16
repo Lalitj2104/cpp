2024874a419917f974890adb74f175d039b9fe97
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-16 14:44:17 +0530

    commited

--- ETE/stack1.cpp
+++ ETE/stack1.cpp
@@ -2,20 +2,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-   void print(stack<int> temp){
-    while(!temp.empty()){
-        cout<<temp.top()<<" ";
-        temp.pop();
-    }
-    cout<<endl;
+void print(stack<int> temp)
+{
+  while (!temp.empty())
+  {
+    cout << temp.top() << " ";
+    temp.pop();
+  }
+  cout << endl;
 }
-int main(){
+int main()
+{
   stack<int> st;
   int n;
-  cin>>n;
-  for(int i=0;i<n;i++){
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
     int temp;
-    cin>>temp;
+    cin >> temp;
     st.push(temp);
   }
 }
