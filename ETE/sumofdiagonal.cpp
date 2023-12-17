49cabb78af262de33c962ad5c8069a18a895179e
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-17 21:19:31 +0530

    commited

--- ETE/sumofdiagonal.cpp
+++ ETE/sumofdiagonal.cpp
@@ -1,35 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int z,x;
-    cin>>z>>x;
+int main()
+{
+    int z, x;
+    cin >> z >> x;
     int arr[z][x];
-    for(int i=0;i<z;i++){
-        for(int j=0;j<x;j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            cin >> arr[i][j];
         }
     }
 
-    int sum=0;
-    for(int i=0;i<z;i++){
-        for(int j=0;j<x;j++){
-            if(i==j){
-                sum+=arr[i][j];
-            }
-        }
-        
-    }
-
-    int q=z/2;
-        sum-=arr[q][q];
-    for(int i=0;i<z;i++){
-        for(int j=0;j<x;j++){
-            if(j==x-1){
-                sum+=arr[i][x-i-1];
+    int sum = 0;
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            if (i == j)
+            {
+                sum += arr[i][j];
             }
         }
     }
 
+    int q = z / 2;
+    sum -= arr[q][q];
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            if (j == x - 1)
+            {
+                sum += arr[i][x - i - 1];
+            }
+        }
+    }
 
-    cout << sum<<endl;
+    cout << sum << endl;
 }
