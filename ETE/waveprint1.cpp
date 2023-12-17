a0bf8fcc160c0b8417e44d581a70cad41fbbdb5e
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-17 21:19:57 +0530

    commited

--- ETE/waveprint1.cpp
+++ ETE/waveprint1.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int z,x;
-    cin>>z>>x;
+int main()
+{
+    int z, x;
+    cin >> z >> x;
     int arr[z][x];
-    for(int i=0;i<z;i++){
-        for(int j=0;i<x;j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; i < x; j++)
+        {
+            cin >> arr[i][j];
         }
     }
-    for(int i=0;i<z;i++){
-        if(i%2!=0){
-            for(int j=x-1;j>=0;j--){
-                cout<<arr[i][j]<<" ";
+    for (int i = 0; i < z; i++)
+    {
+        if (i % 2 != 0)
+        {
+            for (int j = x - 1; j >= 0; j--)
+            {
+                cout << arr[i][j] << " ";
             }
         }
-        else{
-            for(int j=0;j<x;j++){
-                cout<<arr[i][j]<<" ";
+        else
+        {
+            for (int j = 0; j < x; j++)
+            {
+                cout << arr[i][j] << " ";
             }
         }
     }
