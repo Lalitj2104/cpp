0c0fae2fb9798881820462713e5a78b68b67a3ff
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-22 23:12:52 +0530

    commited

--- ETE/checkfirstcapitalornot.cpp
+++ ETE/checkfirstcapitalornot.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     string str;
-    getline(cin,str);
-    bool flag=true;
-    for(int i=0;i<str.length();i++){
-        if(i==0 &&isupper(str[i])){
-            flag=true;
+    getline(cin, str);
+    bool flag = true;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (i == 0 && isupper(str[i]))
+        {
+            flag = true;
         }
-        else if(str[i-1]==' ' && !isupper(str[i])){
-            flag=false;
+        else if (str[i - 1] == ' ' && !isupper(str[i]))
+        {
+            flag = false;
         }
     }
 
-    if(flag==true){
-        cout<<"true";
+    if (flag == true)
+    {
+        cout << "true";
     }
-    else{
-        cout<<"false";
+    else
+    {
+        cout << "false";
     }
 }
