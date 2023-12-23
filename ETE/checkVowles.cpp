ab03fdaaa901287ac64fa03617f5cf051e7333e1
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-24 00:54:08 +0530

    commited

--- ETE/checkVowles.cpp
+++ ETE/checkVowles.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     string str;
-    int count=0;
-    getline(cin,str);
-    int size=str.length();
-    for(int i=0;i<size;i++){
-        if(str[i]=='u'||str[i]=='o'||str[i]=='i'||str[i]=='e'||str[i]=='a'||str[i]=='U'||str[i]=='O'||str[i]=='I'||str[i]=='E'||str[i]=='A'){
+    int count = 0;
+    getline(cin, str);
+    int size = str.length();
+    for (int i = 0; i < size; i++)
+    {
+        if (str[i] == 'u' || str[i] == 'o' || str[i] == 'i' || str[i] == 'e' || str[i] == 'a' || str[i] == 'U' || str[i] == 'O' || str[i] == 'I' || str[i] == 'E' || str[i] == 'A')
+        {
             count++;
         }
     }
-    cout<<count<<endl;
+    cout << count << endl;
 }
