23abc4fa5596e4e4b4f9ba4b22a0c1a19e7ad666
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-27 01:00:50 +0530

    commited

--- ETE/frequency.cpp
+++ ETE/frequency.cpp
@@ -1,27 +1,32 @@
-#include<iostream>
-#include<map>
-#include<vector>
+#include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
-void frequency(vector<int> v,int x){
-    map<int ,int> m;
-    for(int i=0;i<x;i++){
+void frequency(vector<int> v, int x)
+{
+    map<int, int> m;
+    for (int i = 0; i < x; i++)
+    {
         m[v[i]]++;
     }
-    for(auto i:m){
-        cout<<i.first<<" "<<i.second<<endl;
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
     }
 }
 
-int main(){
+int main()
+{
     vector<int> v;
     int x;
-    cin>>x;
-    for(int i=0;i<x;i++){
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
         int q;
-        cin>>q;
+        cin >> q;
         v.push_back(q);
     }
 
-    frequency(v,x);
+    frequency(v, x);
 }
