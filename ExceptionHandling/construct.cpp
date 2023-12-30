434bedec428dd857a33a4a038cb8bb0d36acea4f
Lalit2104 <lalitjindal0295@gmail.com>
2023-12-30 17:51:47 +0530

    commited

--- ExceptionHandling/construct.cpp
+++ ExceptionHandling/construct.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int  x=10,y=0,z;
-    try{
-        if(y==0){
+int main()
+{
+    int x = 10, y = 0, z;
+    try
+    {
+        if (y == 0)
+        {
             throw 1;
         }
-        z=x/y;
-        cout<<z<<endl;
+        z = x / y;
+        cout << z << endl;
     }
-    catch(int q){
-        cout<<"Division by zero "<<q<<endl;
-
+    catch (int q)
+    {
+        cout << "Division by zero " << q << endl;
     }
-
 }
