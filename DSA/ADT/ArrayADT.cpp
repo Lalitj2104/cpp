be1e7ffd86a9de349defdee8b39a91746fbd921f
Lalit2104 <lalitjindal0295@gmail.com>
2024-01-03 21:37:41 +0530

    commited

--- DSA/ADT/ArrayADT.cpp
+++ DSA/ADT/ArrayADT.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 using namespace std;
+// class arr
+// {
+// public:
+//     int *A;
+//     int size;
+//     int length;
+
+// public:
+//     arr()
+//     {
+//         size = 10;
+//         length = 0;
+//         A = new int[size];
+//     }
+//     arr(int z)
+//     {
+//         size = z;
+//         length = 0;
+//         A = new int[z];
+//     }
+
+//     ~arr
+//     {
+//         delete[] A;
+//     }
+// };
+
+// void display(arr a)
+// {
+//     for (int i = 0; i < arr.size; i++)
+//     {
+//         cout << a.A[i];
+//     }
+// }
+
 int main()
 {
-    
+    arr a;
+    int n;
+    cout << "Enter size of an array: " << endl;
+    cin >> &a.size;
+    a.A = new int[arr.size];
+    a.length = 0;
+
+    cout << "Enter elements in array: " << endl;
+
+    for (int i = 0; i < a.size; i++)
+    {
+        cin >> &a.A[i];
+    }
+    a.length = n;
+
+    display(a);
 }
