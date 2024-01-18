0fc00195554340b7b36a2de4d511a7cf6afa4ebf
Lalit2104 <lalitjindal0295@gmail.com>
2024-01-18 19:15:23 +0530

    commited

--- oops/diagonalMartix.cpp
+++ oops/diagonalMartix.cpp
@@ -5,6 +5,10 @@ class diagonal{
     int n;
     int *A;
     public:
+    diagonal(){
+        n=2;
+        A=new int [2];
+    }
     diagonal(int n){
         this->n=n;
         A=new int[n];
@@ -12,12 +16,43 @@ class diagonal{
     void set(int i,int j,int x);
     int get(int i,int j);
     void Display();
-    ~diagonal();
+    ~diagonal(){
+        delete []A;
+    }
 };
 
 void diagonal::Display(){
-    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                cout<<A[i]<<" ";
+            }
+            else{
+                cout<<"0 ";
+            }
+        }
+        cout<<endl;
+    }    
 }
-int main(){
+void diagonal::set(int i,int j,int x){
+    if(i==j){
+        A[i-1]=x;
+    }
+}
+int diagonal::get(int i,int j){
+    if(i==j){
+        return A[i-1];
+    }
+    return 0;
+}
 
+int main(){
+    diagonal d(4);
+
+    d.set(1,1,5);
+    d.set(2,2,6);
+    d.set(3,3,7);
+    d.set(4,4,8);
+
+    d.Display();
 }
