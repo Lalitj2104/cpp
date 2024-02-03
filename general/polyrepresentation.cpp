6be33f8d1bd434ceadf2325953c372a767493452
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-03 20:49:40 +0530

    commited

---
+++ general/polyrepresentation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class term{
+    private:
+    int coeff;
+    int exp;
+};
+
+class poly{
+    private:
+    int n;
+    term *t;
+    public:
+    
+};
