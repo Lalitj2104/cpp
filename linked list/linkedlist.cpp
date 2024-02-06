0dac4e816af4370844d26ae277eae085ec2ef5a2
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-06 08:53:57 +0530

    commited

--- linked list/linkedlist.cpp
+++ linked list/linkedlist.cpp
@@ -1,2 +1,38 @@
 #include<iostream>
 using namespace std;
+class node{
+    public:
+    int data;
+    node * next;
+}*first=NULL;
+
+void display(node *p){
+    while(p!= NULL){
+       cout<<p->data<<" ";
+       p=p->next; 
+    }
+}
+
+void create(int A[],int n){
+    int i;
+    node *t,*last;
+    first=new node;
+    first -> data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(i=1;i<n;i++){
+        t=new node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+int main(){
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    display(first);
+
+}
