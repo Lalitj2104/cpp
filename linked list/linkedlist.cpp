08c9c502c15f5764a8f39b39b25b4c80f21bd543
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-09 23:58:06 +0530

    commited

--- linked list/linkedlist.cpp
+++ linked list/linkedlist.cpp
@@ -1,76 +1,47 @@
 #include <iostream>
 using namespace std;
-// class node{
-//     public:
-//     int data;
-//     node * next;
-// }*first=NULL;
-
-// void display(node *p){
-//     while(p!= NULL){
-//        cout<<p->data<<" ";
-//        p=p->next;
-//     }
-// }
-
-// // recursive function for display
-// // void display(node *p){
-// //     if(p!= NULL){
-// //        cout<<p->data<<" ";
-// //        display(p->next);
-// //     }
-// // }
-
-// void create(int A[],int n){
-//     int i;
-//     node *t,*last;
-//     first=new node;
-//     first -> data=A[0];
-//     first->next=NULL;
-//     last=first;
-
-//     for(i=1;i<n;i++){
-//         t=new node;
-//         t->data=A[i];
-//         t->next=NULL;
-//         last->next=t;
-//         last=t;
-//     }
-// }
-
-// int main(){
-//     int A[]={1,2,3,4,5};
-//     create(A,5);
-//     display(first);
-
-// }
-
-class node
-{
-public:
+class node{
+    public:
     int data;
-    node* next;
+    node * next;
+}*first=NULL;
 
-    node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
+void display(node *p){
+    while(p!= NULL){
+       cout<<p->data<<" ";
+       p=p->next;
     }
-};
-
-void insert(node* &head,int d){
-    node * temp=new node(d);
-    temp->next=head;
-    head=temp;
 }
 
-void print(){
-        
+// recursive function for display
+// void display(node *p){
+//     if(p!= NULL){
+//        cout<<p->data<<" ";
+//        display(p->next);
+//     }
+// }
+
+void create(int A[],int n){ 
+    int i;
+    node *t,*last;
+    first=new node;
+    first -> data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(i=1;i<n;i++){
+        t=new node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
 }
 
-int main()
-{
-    node* node1 = new node(10);
-    cout << node1->data << endl;
-    cout << node1->next << endl;
+int main(){
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    display(first);
+
 }
+
