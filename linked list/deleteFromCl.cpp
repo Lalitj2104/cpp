94581c5d9036604d4928700d20b13bf5c1e3b3f3
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-21 09:00:28 +0530

    commited

--- linked list/deleteFromCl.cpp
+++ linked list/deleteFromCl.cpp
@@ -1,8 +1,88 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-class node{
-    public:
+class node
+{
+public:
     int data;
     node *next;
-}*first;
+} *first = NULL;
+
+void display(node *p){
+
+    do{
+       cout<<p->data<<" ";
+       p=p->next;
+    }while(p!= first);
+    cout<<endl;
+}
+
+int deleteL(node *p, int pos)
+{
+    int i, x;
+    node *q;
+    if (pos <= 0)
+    {
+        return -1;
+    }
+    if (pos == 1)
+    {
+        while (p->next != first)
+        {
+            p = p->next;
+        }
+        x = first->data;
+        if (p == first)
+        {
+            delete first;
+            first = NULL;
+        }
+
+        else
+        {
+            p->next = first->next;
+            delete first;
+            first = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < pos - 2; i++)
+        {
+            p = p->next;
+        }
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        delete q;
+    }
+    return x;
+}
+
+void create(int A[], int n)
+{
+    int i;
+    node *t, *last;
+    first = new node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+    last->next=first;
+}
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5};
+    create(A, 5);
+    display(first);
+    int q = deleteL(first, 0);
+    display(first);
+}
--- linked list/tempCodeRunnerFile.cpp
+++ linked list/tempCodeRunnerFile.cpp
@@ -1,1 +1,89 @@
-    insert(first, 0, 10);
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+} *first = NULL;
+
+void display(node *p)
+{
+    while (p !=first)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int deleteL(node *p, int pos)
+{
+    int i, x;
+    node *q;
+    if (pos < 0)
+    {
+        return -1;
+    }
+    if (pos == 1)
+    {
+        while (p->next != first)
+        {
+            p = p->next;
+        }
+        x = first->data;
+        if (p == first)
+        {
+            free(first);
+            first = NULL;
+        }
+
+        else
+        {
+            p->next = first->next;
+            free(first);
+            first = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < pos - 2; i++)
+        {
+            p = p->next;
+        }
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        delete q;
+    }
+    return x;
+}
+
+void create(int A[], int n)
+{
+    int i;
+    node *t, *last;
+    first = new node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+    last->next=first;
+}
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5};
+    create(A, 5);
+    display(first);
+    int q = deleteL(first, 1);
+    display(first);
+}
