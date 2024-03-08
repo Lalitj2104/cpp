d3d6a7a89d796765fa9ef98c3b8c21be371b342f
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-08 21:46:44 +0530

    commited

--- stack/LLStack2.cpp
+++ stack/LLStack2.cpp
@@ -1,61 +1,71 @@
 #include <iostream>
 using namespace std;
 
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node * next;
+    node *next;
 };
-class stack{
+class stack
+{
     node *top;
-    public:
-    stack(){top=NULL;}
+
+public:
+    stack() { top = NULL; }
     void push(int x);
     void pop();
     void display();
-
 };
-void stack::push(int x){
-    node *temp=new node;
-    if(temp==NULL){
-        cout<<"stack is full"<<endl;
+void stack::push(int x)
+{
+    node *temp = new node;
+    if (temp == NULL)
+    {
+        cout << "stack is full" << endl;
     }
-    else{
-        temp->data=x;
-        temp->next=top;
-        top=temp;
+    else
+    {
+        temp->data = x;
+        temp->next = top;
+        top = temp;
     }
 }
 
-void stack::pop(){
-    int x=-1;
-    if(top==NULL){
-        cout<<"stack is empty"<<endl;
+void stack::pop()
+{
+    int x = -1;
+    if (top == NULL)
+    {
+        cout << "stack is empty" << endl;
     }
-    else{
-        x=top->data;
-        node *t=top;
-        top=top->next;
+    else
+    {
+        x = top->data;
+        node *t = top;
+        top = top->next;
         delete t;
     }
-    cout<<"deleted element is: "<<x<<endl;
+    cout << "deleted element is: " << x << endl;
 }
 
-void stack::display(){
-    node *t=top;
-    while(t!=NULL){
-        cout<<t->data<<" ";
-        t=t->next;
+void stack::display()
+{
+    node *t = top;
+    while (t != NULL)
+    {
+        cout << t->data << " ";
+        t = t->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main(){
+int main()
+{
     stack s;
     s.push(10);
     s.push(20);
     s.push(30);
     s.display();
     s.pop();
-
 }
