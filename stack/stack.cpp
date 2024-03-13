8400103e3a3f0978314a163ea6cc7f04ceb589be
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-13 18:13:51 +0530

    commited

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class stack{
+class stack
+{
     int size;
     int top;
     int *s;
-    public:
-    stack(){
-        size=8;
-        top=-1;
-        s=new int [size];
+
+public:
+    stack()
+    {
+        size = 8;
+        top = -1;
+        s = new int[size];
     }
-    stack(int x){
-        size=x;
-        top=-1;
-        s=new int[size];
+    stack(int x)
+    {
+        size = x;
+        top = -1;
+        s = new int[size];
     }
     void push(int x);
     void pop();
@@ -22,77 +26,89 @@ class stack{
     void stackTop();
     void isEmpty();
     void isFull();
-   
 };
 
-void stack::isEmpty(){
-    if(top==-1){
-        cout<<"Stack is empty"<<endl;
+void stack::isEmpty()
+{
+    if (top == -1)
+    {
+        cout << "Stack is empty" << endl;
         // return 1;
-
     }
-    else{
-        cout<<"Stack is not empty"<<endl;
+    else
+    {
+        cout << "Stack is not empty" << endl;
         // return 0;
     }
 }
-void stack::isFull(){
-    if(top==(size-1)){
-        cout<<"Stack is full"<<endl;
+void stack::isFull()
+{
+    if (top == (size - 1))
+    {
+        cout << "Stack is full" << endl;
         // return 1;
     }
-    else{
-        cout<<"Stack is not full"<<endl;
+    else
+    {
+        cout << "Stack is not full" << endl;
         // return 0;
     }
 }
-void stack::peek(int index){
-    int y=top-index+1;
-    if(y<0){
-        cout<<"Invalid position"<<endl;
-
+void stack::peek(int index)
+{
+    int y = top - index + 1;
+    if (y < 0)
+    {
+        cout << "Invalid position" << endl;
     }
-    else{
-        cout<<s[y]<<endl;
+    else
+    {
+        cout << s[y] << endl;
     }
-
 }
 
-void stack::pop(){
+void stack::pop()
+{
     int q;
-    if(top==-1){
-    cout<<"Stack underflow."<<endl;        
+    if (top == -1)
+    {
+        cout << "Stack underflow." << endl;
     }
-    else{
-        q=s[top];
-        s[top]=0;
+    else
+    {
+        q = s[top];
+        s[top] = 0;
         top--;
-        cout<<"element poped is: "<<q<<endl;
-
+        cout << "element poped is: " << q << endl;
     }
 }
 
-void stack::push(int x){
-    if(top==(size-1)){
-    cout<<"Stack overflow."<<endl;       
+void stack::push(int x)
+{
+    if (top == (size - 1))
+    {
+        cout << "Stack overflow." << endl;
     }
-    else{
-        s[top+1]=x;
+    else
+    {
+        s[top + 1] = x;
         top++;
-
-
     }
 }
-void stack::stackTop(){
-    if(top==-1 ){
-        cout<<"stack is empty"<<endl;
+void stack::stackTop()
+{
+    if (top == -1)
+    {
+        cout << "stack is empty" << endl;
     }
-    else{
-        cout<<"top element is: "<<s[top]<<endl;
+    else
+    {
+        cout << "top element is: " << s[top] << endl;
     }
 }
 
-int main(){
+int main()
+{
     stack s(10);
     s.push(20);
     s.push(30);
