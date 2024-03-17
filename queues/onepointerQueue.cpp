7cc690a0247219a9a41b747809e8e184c80ddb13
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-17 16:23:16 +0530

    commited

--- queues/onepointerQueue.cpp
+++ queues/onepointerQueue.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class queue{
+class queue
+{
     int size;
     int p;
     int *a;
-    public:
-    queue(){
-        size=5;
-        p=-1;
-        a=new int[size];
+
+public:
+    queue()
+    {
+        size = 5;
+        p = -1;
+        a = new int[size];
     }
-    queue(int size){
-        this->size=size;
-        p=-1;
-        a=new int[size];
+    queue(int size)
+    {
+        this->size = size;
+        p = -1;
+        a = new int[size];
     }
     void enqueue(int x);
     void dequeue();
@@ -24,58 +28,70 @@ class queue{
     void display();
 };
 
-void queue:: display(){
-    for(int i=0;i<=p;i++){
-        cout<<a[i]<<" ";
+void queue::display()
+{
+    for (int i = 0; i <= p; i++)
+    {
+        cout << a[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void queue::enqueue(int x){
-    if(isFull()){
-        cout<<"Queue is full"<<endl;
+void queue::enqueue(int x)
+{
+    if (isFull())
+    {
+        cout << "Queue is full" << endl;
     }
-    else{
-        a[p+1 ]=x;
+    else
+    {
+        a[p + 1] = x;
         p++;
     }
 }
-bool queue::isEmpty(){
-    if(p==-1)
+bool queue::isEmpty()
+{
+    if (p == -1)
     {
         return 1;
     }
     return 0;
 }
 
-bool queue::isFull(){
-    if(p==size-1){
+bool queue::isFull()
+{
+    if (p == size - 1)
+    {
         return 1;
     }
     return 0;
 }
 
-void queue::first(){
-    cout<<"the first element in queue is: "<<a[0]<<endl;
+void queue::first()
+{
+    cout << "the first element in queue is: " << a[0] << endl;
 }
 
-void queue::last(){
-    cout<<"the last element in queue is: "<<a[p]<<endl;
-
+void queue::last()
+{
+    cout << "the last element in queue is: " << a[p] << endl;
 }
 
-void queue::dequeue(){
-    if(isEmpty()){
-        cout<<"Queue is empty"<<endl;
+void queue::dequeue()
+{
+    if (isEmpty())
+    {
+        cout << "Queue is empty" << endl;
     }
-    for(int i=0;i<p;i++){
-        a[i]=a[i+1];
-
+    for (int i = 0; i < p; i++)
+    {
+        a[i] = a[i + 1];
     }
     p--;
 }
 
-int main(){
+int main()
+{
     queue s(5);
     s.enqueue(1);
     s.enqueue(2);
