80604f7fe9f72dc88d87a2b92426ea444ae9d206
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-25 00:01:39 +0530

    commited

--- recursion/reversestring.cpp
+++ recursion/reversestring.cpp
@@ -3,27 +3,26 @@ using namespace std;
 void ReverseString(string &arr, int i, int j)
 {
 
-    
-     if (i >j)
+    if (i > j)
     {
         return;
     }
-    
+
     swap(arr[i], arr[j]);
     i++;
     j--;
-    
-    ReverseString(arr , i, j );
+
+    ReverseString(arr, i, j);
 }
 
 int main()
 {
 
     string arr;
-    cout<<"Enter the string: "<<endl;
-     cin >> arr;
-    int j=arr.length()-1;
-    ReverseString(arr,0, j);
+    cout << "Enter the string: " << endl;
+    cin >> arr;
+    int j = arr.length() - 1;
+    ReverseString(arr, 0, j);
 
-    cout<<arr;
+    cout << arr;
 }
