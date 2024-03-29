e430414925b7a99af1df8e033731662bcdde955d
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-29 13:44:42 +0530

    commited

---
+++ recursion/arraySubWithCondition.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void printSubsequences(int arr[], int index, vector<int> &subarr,int n,int sum,int s)
+{
+	
+	if (index == n)
+	{
+        if(s==sum){
+		for (auto it:subarr){
+		cout << it << " ";
+		
+		}
+        cout<<endl;
+        return;
+        }
+
+		
+	}
+	else
+	{
+
+		subarr.push_back(arr[index]);
+	
+     sum+=arr[index];
+		printSubsequences(arr, index + 1, subarr,n,sum,s);
+
+		sum-=arr[index];
+		subarr.pop_back();
+        
+
+        printSubsequences(arr, index + 1, subarr,n,sum,s);
+		
+	}
+	
+}
+
+int main()
+{
+	int arr[]={1,2,1 };
+	int n=sizeof(arr)/sizeof(arr[0]);
+	vector<int> vec;
+	int sum =2;
+
+	printSubsequences(arr, 0, vec,n,0,sum);
+
+	return 0;
+}
